3d963fe12cbd552b69566bfac79a68564dab5c3a
Wojciech Bojarczuk <bojarczuk@protonmail.com>
2018-09-07 10:22:35 +0200

    Add project files

---
+++ client/modules/Lane/Lanes.js
@@ -0,0 +1,18 @@
+import React, { PropTypes } from 'react';
+import Lane from './LaneContainer.js';
+import styles from './Lane.css';
+
+
+const Lanes = ({ lanes }) => {
+  return (
+    <div className={styles.Lanes}>{lanes.map(lane =>
+      <Lane key={lane.id} lane={lane} />
+    )}</div>
+  );
+};
+
+Lanes.propTypes = {
+  lanes: PropTypes.array,
+};
+
+export default Lanes;
---
+++ client/modules/Note/Note.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+import styles from './Note.css';
+
+const Note = props =>
+  <li className={styles.Note}>{props.children}</li>;
+
+Note.propTypes = {
+  children: PropTypes.any,
+};
+
+export default Note;
