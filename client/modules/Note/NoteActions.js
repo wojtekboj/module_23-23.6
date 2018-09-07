0eb030d7594110681f717c9f22ba10a1d994ae31
Wojciech Bojarczuk <bojarczuk@protonmail.com>
2018-09-07 22:39:10 +0200

    Modification NoteActions.js

--- client/modules/Note/NoteActions.js
+++ client/modules/Note/NoteActions.js
@@ -4,7 +4,7 @@ import callApi from '../../util/apiCaller';
 export const CREATE_NOTE = 'CREATE_NOTE';
 export const UPDATE_NOTE = 'UPDATE_NOTE';
 export const DELETE_NOTE = 'DELETE_NOTE';
-export const CREATE_NOTES = 'CREATE_LANES';
+export const CREATE_NOTES = 'CREATE_NOTES';
 export const EDIT_NOTE = 'EDIT_NOTE';
 
 // Export Actions
